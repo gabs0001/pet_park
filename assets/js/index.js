bb3b9bea65ca2dcb40d01ca10e823b42109bf248
gabriel <silvagab566@gmail.com>
2025-07-28 16:20:07 -0300

    primeira versão do pet_park

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+const btn_left = document.querySelector('#btn_left')
+const btn_right = document.querySelector('#btn_right')
+const carrosel_images = document.querySelectorAll('.carrosel-imagem')
+
+btn_left.addEventListener('click' , () => {
+    
+})
+
+btn_right.addEventListener('click' , () => {
+    
+})
